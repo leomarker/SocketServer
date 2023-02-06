9d6e6b4f37b77e579ae18b30ec19111b85798023
Nati <natnael3370@gmail.com>
2023-02-06 03:42:30 +0300

    adding a thread in to the app

--- src/MultipleClient.java
+++ src/MultipleClient.java
@@ -1,6 +1,7 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MultipleClient  extends Thread{
     private ServerSocket serverSocket ;
@@ -9,16 +10,17 @@ public class MultipleClient  extends Thread{
 
     public MultipleClient(int port){
         this.port = port;
-
     }
 
 
     public  void startServer() throws IOException {
         serverSocket = new ServerSocket( port);
+        System.out.println("server started");
         this.start();
+        running = true;
     }
 
-    public void stopeServer(){
+    public void stopServer(){
         this.interrupt();
         running = false;
     }
@@ -36,8 +38,70 @@ public class MultipleClient  extends Thread{
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+         MultipleClient multipleClient = new MultipleClient(8080);
+         multipleClient.startServer();
+        System.out.println("server");
+        try
+        {
+            Thread.sleep( 60000 );
+        }
+        catch( Exception e )
+        {
+            e.printStackTrace();
+        }
+
+        multipleClient.stopServer();
+    }
+
+}
+
+class RequestHandler extends Thread{
+    private Socket socket;
+
+    RequestHandler(Socket socket){
+        this.socket = socket;
+    }
+
+    public void run() throws RuntimeException {
+
+        System.out.println("we got a connection");
+
+
+        InputStream is = null;
+        try {
+            is = socket.getInputStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        OutputStream os = null;
+        try {
+            os = socket.getOutputStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        DataInputStream dis = new DataInputStream(is);
+        DataOutputStream dos = new DataOutputStream(os);
+
+        System.out.print("server:");
+        String st = "server:"+ new Scanner(System.in).nextLine();
+        String line ;
+
+        try {
+            line = dis.readUTF();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            dos.writeUTF(st);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(line);
+
 
     }
 }
-
